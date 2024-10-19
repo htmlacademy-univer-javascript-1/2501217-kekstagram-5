281f3ab851a8ba7f6c174e9d6b479eafcddbd8eb
Илья Муравьев <ilyamuravyov@mail.ru>
2024-10-19 16:13:28 +0500

    Отрисуй меня полностью

--- js/draw-pictures.js
+++ js/draw-pictures.js
@@ -5,17 +5,17 @@ const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const photos = createPhotos();
-const photosListFragment = document.createDocumentFragment();
+const pictures = createPhotos();
+const picturesListFragment = document.createDocumentFragment();
 
-photos.forEach(({url, description, likes, comments}) => {
+pictures.forEach(({url, description, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   const pictureImage = pictureElement.querySelector('.picture__img');
   pictureImage.src = url;
   pictureImage.alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  photosListFragment.appendChild(pictureElement);
+  picturesListFragment.appendChild(pictureElement);
 });
 
-picturesListElement.appendChild(photosListFragment);
+picturesListElement.appendChild(picturesListFragment);
